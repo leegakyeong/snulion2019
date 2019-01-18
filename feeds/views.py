12b121a28f3a07a4a8ace3d24144055ce3067342
unknown <alaska7132@naver.com>
2019-01-19 01:06:09 +0900

    1.1. create

--- feeds/views.py
+++ feeds/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from .models import Feed
+from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
-    feeds = Feed.objects.all()
-    return render(request, 'feeds/index.html', {'feeds': feeds})
+    if request.method == 'GET': # index
+        feeds = Feed.objects.all()
+        return render(request, 'feeds/index.html', {'feeds': feeds})
+    elif request.method == 'POST': # create
+        title = request.POST['title']
+        content = request.POST['content']
+        Feed.objects.create(title=title, content=content)
+        return redirect('/feeds')
 
 def new(request):
     return render(request, 'feeds/new.html', {})
