47a6d3f27a1a8e15a2ba94ae85c0c130f9481b6d
unknown <alaska7132@naver.com>
2019-01-19 00:45:55 +0900

    1.1. View

--- feeds/views.py
+++ feeds/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Feed
 
 # Create your views here.
+def index(request):
+    feeds = Feed.objects.all()
+    return render(request, 'feeds/index.html', {'feeds': feeds})
