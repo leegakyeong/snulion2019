2bc43b555385d3f594f37e9c278d6c8ad5f43e0e
yeongrok.jeong <yeongrok.jeong@gmail.com>
2019-01-19 18:01:16 +0900

    Generic View & Django auth (~login, logout)

--- feeds/admin.py
+++ feeds/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Feed
+from .models import FeedComment
 
 # Register your models here.
+admin.site.register(Feed)
+admin.site.register(FeedComment)
--- feeds/urls.py
+++ feeds/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from feeds import views
 
 urlpatterns = [
-    path('', views.index, name='index'), # name은 url을 변수로 쓰기 위해서 지정해주는 것
-    path('new/', views.new, name='new'),
-    path('<int:id>/', views.show, name='show'),
-    path('<int:id>/edit', views.edit, name='edit'),
-    path('<int:id>/delete', views.delete, name='delete'),
+    # .as_view()는 아직 클래스의 인스턴스가 없어서, 자동으로 인스턴스를 만들어주는 거다.
+    # queryset은 구체적이고 명확하게 말해줄 때. model = Feed는 queryset으로 풀어쓰면 queryset = Feed.objects.all
+    path('', views.FeedListView.as_view(), name='index'), # name은 url을 변수로 쓰기 위해서 지정해주는 것
+    path('new/', views.FeedCreateView.as_view(), name='new'),
+    path('<int:pk>/', views.FeedDetailView.as_view(), name='show'), #pk는 primary key
+    path('<int:pk>/edit', views.FeedUpdateView.as_view(), name='edit'),
+    path('<int:pk>/delete', views.FeedDeleteView.as_view(), name='delete'),
     path('<int:id>/comments/', views.create_comment, name='create_comment'),
     path('<int:id>/comments/<int:cid>/', views.delete_comment, name='delete_comment'),
 ]
