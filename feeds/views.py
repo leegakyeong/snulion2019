37ad3aaa47687b8524377d531c49784cdc2258bb
hwan517h1 <hwan517h1@gmail.com>
2019-01-19 16:31:02 +0900

    이가경 발표 내용 구현

---
+++ feeds/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from feeds.models import Feed, FeedComment
+from django.shortcuts import redirect
+
+def index(request):
+    if request.method == 'GET': # index
+        feeds = Feed.objects.all()
+        return render(request, 'feeds/index.html', {'feeds': feeds})
+    elif request.method == 'POST': # create
+        title = request.POST['title']
+        content = request.POST['content']
+        Feed.objects.create(title=title, content=content)
+        return redirect('/feeds')
+
+def new(request):
+    return render(request, 'feeds/new.html', {})
+
+def show(request, id):
+    if request.method == 'GET': # show
+        feed = Feed.objects.get(id=id)
+        return render(request, 'feeds/show.html', {'feed': feed})
+    elif request.method == 'POST': # update
+        title = request.POST['title']
+        content = request.POST['content']
+        feed = Feed.objects.get(id=id)
+        feed.title = title
+        feed.content = content
+        feed.save()
+        feed.update_date()
+        return redirect('/feeds/' + str(id))
+
+def edit(request, id):
+    feed = Feed.objects.get(id=id)
+    return render(request, 'feeds/edit.html', {'feed': feed})
+
+def delete(request, id):
+    feed = Feed.objects.get(id=id)
+    feed.delete()
+    return redirect('/feeds')
+
+def create_comment(request, id):
+    content = request.POST['content']
+    FeedComment.objects.create(feed_id=id, content=content)
+    return redirect('/feeds')
+
+def delete_comment(request, id, cid):
+    c = FeedComment.objects.get(id=cid)
+    c.delete()
+    return redirect('/feeds')
