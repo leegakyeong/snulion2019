616a691d962a3288b163718ac82c081f7e2ec341
yeongrok.jeong <yeongrok.jeong@gmail.com>
2019-01-19 15:55:22 +0900

    CRUD

--- feeds/urls.py
+++ feeds/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from feeds import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'), # name은 url을 변수로 쓰기 위해서 지정해주는 것
+    path('new/', views.new, name='new'),
+    path('<int:id>/', views.show, name='show'),
+    path('<int:id>/edit', views.edit, name='edit'),
+    path('<int:id>/delete', views.delete, name='delete'),
 ]
--- feeds/views.py
+++ feeds/views.py
@@ -1,7 +1,40 @@
 from django.shortcuts import render
 from .models import Feed
+from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
-    feeds = Feed.objects.all()
-    return render(request, 'feeds/index.html', {'feeds': feeds})
+    if request.method == 'GET': # index
+        feeds = Feed.objects.all()
+        return render(request, 'feeds/index.html', {'feeds': feeds})
+    elif request.method == 'POST': # create
+        title = request.POST['title']
+        content = request.POST['content']
+        Feed.objects.create(title=title, content=content)
+        return redirect('/feeds')
+
+def new(request):
+    return render(request, 'feeds/new.html', {})
+
+def show(request, id):
+    if request.method == 'GET':
+        feed = Feed.objects.get(id=id)
+        return render(request, 'feeds/show.html', {'feed': feed})
+    elif request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        feed = Feed.objects.get(id=id)
+        feed.title = title
+        feed.content = content
+        feed.save()
+        feed.update_date()
+        return redirect('/feeds/' + str(id))
+
+def edit(request, id):
+    feed = Feed.objects.get(id=id)
+    return render(request, 'feeds/edit.html', {'feed': feed})
+
+def delete(request, id):    # delete라는 메소드가 이미 있으니까 사실 이 함수 만들 때는 'delete'라는 이름은 피해줘야 함
+    feed = Feed.objects.get(id=id)
+    feed.delete()
+    return redirect('/feeds')
