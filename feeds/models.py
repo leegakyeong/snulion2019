d7db2f23c84b6db8ae79f09274bcc636d82579ab
unknown <alaska7132@naver.com>
2019-01-19 17:16:18 +0900

    2.2. Generic View - POST (+ import reverse 빼먹은 거 )추가

--- feeds/models.py
+++ feeds/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 
+from django.urls import reverse
+
 # Create your models here.
 class Feed(models.Model):
     title = models.CharField(max_length=256)
