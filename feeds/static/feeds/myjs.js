07916857dd2a2947607223a7cfa486ad6f917fd5
Jaewoo Maeng <maeng9584@gmail.com>
2019-02-23 03:25:58 +0900

    ajax final

--- feeds/static/feeds/myjs.js
+++ feeds/static/feeds/myjs.js
@@ -1,12 +1,4 @@
 $(document).ready(() => {
-    $('.button-a').on('click', () => {
-        $('.comment-toggle-a').hide();
-        $('.comment-toggle-b').show();
-    })
-    $('.button-b').on('click', () => {
-        $('.comment-toggle-b').hide();
-        $('.comment-toggle-a').show();
-    })
     // for csrf_token
     $(function() {
 
